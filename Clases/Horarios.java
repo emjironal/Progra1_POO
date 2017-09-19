a566a89b14a4f67f78634ed8daa42d99e856be99
emjironal <31808661+emjironal@users.noreply.github.com>
2017-09-19 09:23:12 -0600

    Update Horarios.java

--- Clases/Horarios.java
+++ Clases/Horarios.java
@@ -4,11 +4,22 @@
  * and open the template in the editor.
  */
 package progra1;
-
+import java.time.*;
+import java.util.ArrayList;
 /**
  *
  * @author Eduardo
  */
 public class Horarios {
+    ArrayList<LocalDateTime> inicio;
+    ArrayList<LocalDateTime> fin;
     
+    public Horarios(int year,int 
 }
+
+NO TOCAR
+http://www.oracle.com/technetwork/es/articles/java/paquete-java-time-2390472-esa.html
+http://docs.oracle.com/javase/8/docs/api/java/time/DayOfWeek.html
+http://docs.oracle.com/javase/8/docs/api/java/time/LocalDate.html
+http://docs.oracle.com/javase/8/docs/api/java/time/LocalTime.html
+http://docs.oracle.com/javase/8/docs/api/java/time/LocalDateTime.html
